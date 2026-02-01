93e39f406f44d0be3c07eb04dfcb3703649e1d38
Dave McCollough <davemccollough@gmail.com>
2026-01-31 17:38:30 -0700

    Chapter 2 notes - part 1

---
+++ 0x200/01_first_program/firstprog.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+int main()
+{
+    int i;
+    for(i=0; i<20; i++)
+    {
+        puts("Hello, World!\n");
+    }
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Hacking: The Art of Exploitation
 Projects and examples from Jon Erickson's black and orange book.
 
+- [Chapter 0x200](0x200)
+
 
